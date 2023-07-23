236bfdea71cf90cc78ccbf97bfee7ac8358e1ad8
Gandarapu Vishnu <gv1shnu@outlook.com>
2023-07-23 18:54:21 +0530

    Update genre2plot.py

--- genre2plot.py
+++ genre2plot.py
@@ -1,9 +1,1 @@
-import pickle
 
-hm_pickle = "db/hashmap.pickle"
-try:
-    with open(hm_pickle, "rb") as file:
-        hashmap = pickle.load(file)
-except FileNotFoundError:
-    with open(hm_pickle, "wb") as file:
-        pickle.dump({}, file)
